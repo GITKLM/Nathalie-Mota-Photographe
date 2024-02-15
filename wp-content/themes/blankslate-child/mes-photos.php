57eeb7a0aaf799a169c9e4cb9fdb8b07d71cd336
GITKLM <152407061+GITKLM@users.noreply.github.com>
2024-02-15 19:12:56 +0400

    template articles

--- wp-content/themes/blankslate-child/mes-photos.php
+++ wp-content/themes/blankslate-child/mes-photos.php
@@ -2,16 +2,26 @@
 // Affichage personnalisé pour les articles individuels
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
+<article id="post-<?php the_ID(); ?>" <?php post_class('flex-container'); ?>>
+    <header class="entry-header flex-item">
+        <h2 class="entry-title"><?php the_title(); ?></h2>
         <div class="entry-meta">
-            <span class="posted-on"><?php echo get_the_date(); ?></span>
-            <span class="byline"><?php esc_html_e( 'by', 'text-domain' ); ?> <?php the_author(); ?></span>
+        <p>REFERENCE: <?php echo get_post_meta( get_the_ID(), 'reference', true ); ?></p>
+            <p>CATEGORIE: <?php echo get_the_category_list( ', ' ); ?></p>
+            <p>FORMAT: <?php echo get_post_format( get_the_ID() ); ?></p>
+            <p>TYPE: <?php echo get_post_type( get_the_ID() ); ?></p>
+            <p>ANNEE: <?php echo get_post_meta( get_the_ID(), 'annee', true ); ?></p>
+            <img src="<?php echo get_stylesheet_directory_uri() . '/images/Line 3.png'; ?>" alt="petite ligne">
+            <div class="contact-container">
+                <p>Cette photo vous intéresse ?</p>
+                <button id="btnContact">CONTACT</button>
+            </div>
+            <!-- <span class="posted-on"><?php echo get_the_date(); ?></span>
+            <span class="byline"><?php esc_html_e( 'by', 'text-domain' ); ?> <?php the_author(); ?></span> -->
         </div>
     </header><!-- .entry-header -->
 
-    <div class="entry-content">
+    <div class="entry-content flex-item">
         <?php the_content(); ?>
     </div><!-- .entry-content -->
 
@@ -20,4 +30,10 @@
             <?php comments_template(); ?>
         </div><!-- .comments-area -->
     <?php endif; ?>
+
+
 </article><!-- #post-<?php the_ID(); ?> -->
+<footer class="footer">
+    <?php get_template_part( 'nav', 'below-single' ); ?>
+</footer>
+<?php get_footer(); ?>
--- wp-content/themes/blankslate-child/single.php
+++ wp-content/themes/blankslate-child/single.php
@@ -3,7 +3,4 @@
     <?php //ajout du template qui gère les articles
     get_template_part( 'mes-photos' ); ?>
 <?php endwhile; endif; ?>
-<footer class="footer">
-    <?php get_template_part( 'nav', 'below-single' ); ?>
-</footer>
-<?php get_footer(); ?>
+
